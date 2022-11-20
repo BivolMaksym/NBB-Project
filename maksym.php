3b67d3242071e545d6c53e9266531e95f926a418
Bivol Maksym <maksym.bivol@student.nhlstenden.com>
2022-11-20 20:44:25 +0100

    Worked on maksyms page

---
+++ maksym.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Maksym Bivol</title>
+</head>
+<body>
+
+</body>
+</html>
