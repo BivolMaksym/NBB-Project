98685618cc2890d122139ce2fb3ed3cfbe4a168a
Clarissa003 <clarissa.dobircianu@student.nhlstenden.com>
2022-11-24 17:39:07 +0100

    I completed my page for the website

---
+++ clarissa.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="css/clarissa.css">
+    <title>Clarissa</title>
+</head>
+<body>
+	 <div id="mainContainer">
+		<header>
+		<div id="title">
+			<img src="img/clarissa/italy.jpeg" height="100" width="150">
+			<a href="main.php">
+			<img src="img/NHL_logo.png" height="140" width="150">
+			</a>
+			<img src="img/clarissa/romania.jpeg" height="100" width="150">
+			</div>
+			<h1>Clarissa Dobircianu</h1>
+		
+			
+		</header>
+		
+		<main>
+	
+			
+			<div class="info">
+			<a href="https://www.youtube.com/watch?v=M7OX6YwmTj4">
+			<img src="img/clarissa/les.jpeg" width="200" height="200">
+			</a>
+			<p><strong>Favourite song</strong></p>
+			<p>Les by Childish Gambino</p>
+			</div>
+			
+			<div class="info">
+			<img src="img/clarissa/pizza.jpeg"  width="200" height="200">
+			<p><strong>Favourite food</strong></p>
+			<p>Pizza</p>
+			</div>
+			
+			<div class="info">
+			<img src="img/clarissa/sandra.jpeg"  width="200" height="200" >
+			<p><strong>Favourite actress</strong></p>
+			<p>Sandra Bullock</p>
+			</div>
+			
+			<div class="info">
+			<img src="img/clarissa/karate.jpeg" width="200" height="200">
+			<p><strong>Activity I did</strong></p>
+			<p>Karate</p>
+			</div>
+			
+			<div class="info">
+			<img src="img/clarissa/bobby.jpg" width="200" height="200">
+			<p><strong>My dog</strong></p>
+			<p>Bobby</p>
+			</div>
+			
+			<div class="info">
+			<img src="img/clarissa/verola.jpeg" width="200" height="200">
+			<p><strong>My hometown</strong></p>
+			<p>Verolavecchia</p>
+			</div>
+			
+			<div class="box">
+			<h1>Random facts about me</h1>
+			<p>1.My goal is to work in cybersecurity</p>
+			<p>2.A country I hope to visit soon? Japan</p>
+			<p>3.I know how to rap 2pac's "Hit em up" word by word</p>
+			<p>4.I can speak fluently 3 languages: Romanian,Italian and English</p>
+			<p>5.Other than karate,I did 4 years of hip-hop and I have high dance skills</p>
+			<p>6.I used to have a soap bars collection when I was a kid and I was very proud of it</p>
+			</div>
+			
+			<div class="box2">
+			<h1>My current obsessions</h1>
+			
+			<div class="boxText">
+			<img src="img/clarissa/crown.jpeg" width="200" height="200">
+			<p>The crown on Netflix</p>
+			</div>
+			
+			<div class="boxText">
+			<img src="img/clarissa/baggy.jpeg" width="200" height="200">
+			<p>Baggy Clothes & style</p>
+			</div>
+			
+			<div class="boxText">
+			<img src="img/clarissa/greek.jpeg" width="200" height="200">
+			<p>Griekse Corner in Emmen</p>
+			</div>
+			</div>
+		</main>
+		
+		<footer>
+		<a href="https://www.facebook.com/"> <img src="img/clarissa/facebook.png" width="50" height="50">
+		</a>
+		<p>Thank you for your attention!</p>
+		<a href="https://www.instagram.com/cladbr/"> <img src="img/clarissa/instagram.png" width="50" height="50">
+		</a>
+		</footer>
+		
+	 </div>
+
+</body>
