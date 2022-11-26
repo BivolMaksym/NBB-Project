ad111984c127cbab43aa631566bde27e0be19847
Clarissa003 <clarissa.dobircianu@student.nhlstenden.com>
2022-11-26 23:48:44 +0100

    I completed the main page according to the design and added its images

---
+++ main.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="css/main.css">
+    <title>Main</title>
+</head>
+<body>
+	 <div id="mainContainer">
+		<header>
+			<img src="img/NHL_logo.png" height="140" width="150">
+			<h1 id="title">Nhl Stenden Students</h1>
+		</header>
+		<main>
+		<div class="students">
+			<a href="clarissa.php">
+			<img src="img/dobircianu.jpeg" width="250" height="250" >
+			<div class="nameBox">
+			<p><strong>Clarissa<br>Dobircianu</strong></p>
+			<img src="img/italy.jpeg" width="50" height="50">
+			</div>
+			</a>
+			</div>
+			
+			<div class="students">
+			<a href="maksym.php">
+			<img src="img/harold.jpeg"  width="250" height="250">
+			<div class="nameBox">
+			<p><strong>Maksym<br>Bivol</strong></p>
+			<img src="img/ukraine.jpeg" width="50" height="50">
+			</div>
+			</a>
+			</div>
+			
+			<div class="students">
+			<a href="anastasia.php">
+			<img src="img/anastasia.jpeg"  width="250" height="250" >
+			<div class="nameBox">
+			<p><strong>Anastasia<br>Lukanova</strong></p>
+			<img src="img/macedonia.jpeg" width="50" height="50">
+			</div>
+			</a>
+			</div>
+		
+		</main>
+	 </div>
+</body>
+</html>
