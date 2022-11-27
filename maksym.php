d98000ae1ab1c0d6499f02c50b7932d26d2e050d
Bivol Maksym <maksym.bivol@student.nhlstenden.com>
2022-11-27 04:16:48 +0100

    Finished maksym page

--- maksym.php
+++ maksym.php
@@ -3,9 +3,66 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/maksym.css" rel="stylesheet">
 	<title>Maksym Bivol</title>
 </head>
 <body>
+	<div id="main">
+		<div class="top">
+			<img src="img/NHL_logo.png">
+			<p class="panim"> Hello, I'm Maksym,</p>
+        </div>
+		<div id ="hero">
+		     <div class="hero1">
+			     <h1>Software<br>Developer #</h1>
+				    <div class="hero12">
+			           <p>from Ukraine<p>
+					   <img class ="flag"src="img/uflag.jpg">
+                    </div>
+             </div>
+			 <div id="hero2">
+                <img src="img/maksym/maksym.jpg">
+             </div>
+        </div>
+		<div id="hero2">
+           <h1>About me.</h1>
+		   <p>I am 17 years old, 1st year student at NHL Stenden based in Netherlands, Emmen.
+			 I study at ICT (Information Communication Technologies) course.
+			   I learn HTML, PHP, MySQL, JavaScript, Java.
+			   Below you can see the list of things I am interested in.
+           </p>
+        </div>
+		<footer>
+			<div class="list">
+			   <h2 class="h2back">Games</h2>
+               <ul>
+				<li><a href="https://www.dota2.com/home">Dota 2<img src="img/maksym/dota.png"></a></li>
+				<li><a href="https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/">CS:GO<img src="img/maksym/csgo.png"></a></li>
+				<li><a href="https://playvalorant.com/">Valorant<img src="img/maksym/valorant.png"></a></li>
+				<li><a href="https://worldofwarcraft.com/">WoW<img src="img/maksym/wow.png"></a></li>
+				<li><a href="https://albiononline.com">Albion<img src="img/maksym/albion.png"></a></li>
+               </ul>
+			</div>
 
+			<div class="list"> 
+			   <h2 class="h2back">Sports</h2>
+			   <ul>
+			   <li><a href="https://www.nba.com/">Basketball<img src="img/maksym/basketball.png"></a></li>
+			   <li><a href="https://www.tennis.com/">Tennis<img src="img/maksym/tennis.png"></a></li>
+			   <li><a href="https://www.karate.com/">Karate<img src="img/maksym/karate.png"></a></li>
+               </ul>
+			</div>
+
+			<div class="list"> 
+			   <h2 class="h2back">Hobbies</h2>
+			   <ul>
+			   <li><a href="https://www.livechart.me/anime/3437">Films/Anime<img src="img/maksym/film.png"></a></li>
+			   <li><a href="https://en.wikipedia.org/wiki/Computer_programming">Coding<img src="img/maksym/console.png"></a></li>
+			   <li><a href="https://open.spotify.com/">Music<img src="img/maksym/music.png"></a></li>
+			   <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWY64wDtewQt?utm_source=generator" width="100%" height="320" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+               </ul>
+			</div>
+        </footer>
+    </div>
 </body>
 </html>
