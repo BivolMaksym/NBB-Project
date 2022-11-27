bb3722e01d5389e8df4a625e9460180175fa1329
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2022-11-27 22:28:30 +0100

    final design resit

---
+++ anastasia.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="css/anastasia.css">
+    <title>Anastasia</title>
+</head>
+<body>
+	 <div id="mainContainer">
+		<header>
+			<div id="title">
+				<a href="main.php"><img src="img/NHL_logo.png" height="140" width="150"></a>
+				<img src="img/anastasia/macedonia.jpeg" height="100" width="150">
+			</div>
+			<h1>Anastasia Lukanova</h1>
+		</header>
+		
+		<main>
+			<div class="info">
+				<a href="https://www.youtube.com/watch?v=WuxHWc-ZEXw&list=PLAsrjxt66CYYiEMRFBb7_tm-WbE58WZtM"><img src="img/anastasia/mmfood.jpg" width="200" height="200"></a>
+				<p><strong>Favourite Albums</strong></p>
+				<p>Mm Food by MF DOOM</p>
+			</div>
+			
+			<div class="info">
+				<a href="https://www.youtube.com/watch?v=jNY_wLukVW0&list=PLxzSZG7g8c8x6GYz_FcNr-3zPQ7npP6WF"><img src="img/anastasia/okcomputer.png" width="200" height="200"></a>
+				<p><strong>Favourite Albums</strong></p>
+				<p>OK Computer by Radiohead</p>
+			</div>
+			
+			<div class="info">
+				<a href="https://www.youtube.com/watch?v=fWvKvOViM3g"><img src="img/anastasia/paranoid.jpg" width="200" height="200"></a>
+				<p><strong>Favourite Albums</strong></p>
+				<p>Paranoid by Black Sabbath</p>
+			</div>
+			
+			<div class="info">
+				<img src="img/anastasia/sushi.jpg" width="200" height="200">
+				<p><strong>Favourite Food</strong></p>
+				<p>Sushi</p>
+			</div>
+			
+			<div class="info">
+				<img src="img/anastasia/jjba.jpg" width="200" height="200">
+				<p><strong>Obsession</strong></p>
+				<p>Jojos Bizzare Adventure</p>
+			</div>
+			
+			<div class="info">
+				<img src="img/anastasia/ohrid.jpg" width="200" height="200">
+				<p><strong>My hometown</strong></p>
+				<p>Ohrid, Macedonia</p>
+			</div>
+			
+			<div class="box">
+				<h1>Random facts about me</h1>
+				<p>1. My goal is to graduate uni and work in IT</p>
+				<p>2. I enjoy reading manga, my manga writer is Junji Ito</p>
+				<p>3. I like metal music, but i also really like old school rap</p>
+				<p>4. My favourite colors are purple and black</p>
+				<p>5. i enjoy coding, my favourite programming language is Python</p>
+				<p>6. My favourite genre of movies is horror</p>
+			</div>
+		</main>
+		
+		<footer>
+			<p>Thank you for your attention!</p>
+		</footer>
+	 </div>
+</body>
